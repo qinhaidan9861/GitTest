4b6e6b223e1b41ff5c65a8cfd36fe7bc56e22197
qhd <1756909433@qq.com>
2023-03-01 20:23:18 +0800

    second commit

--- src/main/java/com/qhd/git/GitTest.java
+++ src/main/java/com/qhd/git/GitTest.java
@@ -8,5 +8,6 @@ package com.qhd.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,Git");
+        System.out.println("hello,GitHub");
     }
 }
