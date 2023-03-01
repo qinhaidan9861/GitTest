f96e721d9ea82deed8559bab7a314c8578e1eea9
qhd <1756909433@qq.com>
2023-03-01 20:18:03 +0800

    first commit

---
+++ src/main/java/com/qhd/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.qhd.git;
+
+/**
+ * @author: qhd
+ * @date: 2023-03-01
+ * @description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,Git");
+    }
+}
